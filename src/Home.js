143af34874d09645535225f107a624867137e41e
Louise <louise.peridy22@gmail.com>
2022-10-13 16:13:55 +0200

    styled compenents

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { set_count, set_message, set_des } from './store/actions/actions-types';
+import styled from 'styled-components'
 
 function Home() {
-  // lecture du store de la source de vérité read-only
-  const { messages, count, des } = useSelector(state => state.message);
-  // actions dispatch dans le reducer => newState 
-  const dispatch = useDispatch();
-  console.log(messages, count, des);
+  const { messages, count, des } = useSelector(state => state.message); // on récupère le state du store
+  const dispatch = useDispatch(); // permet d'envoyer des actions au store
+
+  const Title = styled.h1`
+  font-size: 5em;
+  text-align: center;
+  margin-bottom: -8%;
+`;
 
   function verification(var1, var2, var3) {
     if (var1 === var2 && var2 === var3) {
@@ -14,16 +18,17 @@ function Home() {
     }
   }
 
-  function lancer() {
+  function lancer() { // fonction qui va envoyer des actions au store
     dispatch(set_count());
     dispatch(set_des());
   }
 
   return (
     <div className="App">
+      <Title>YAMS</Title>
+      <center><img src='https://thumbs.gfycat.com/ElatedImpartialArmadillo-max-1mb.gif' width="400" alt='Yams' /></center>
       <button onClick={() => lancer()}>Lancer</button>
       <p>Nombre d'essais : {count}</p>
-
 
       {des.map((des, i) =>
         <p key={i}>{des.number1} {des.number2} {des.number3}</p>)
